7b1d957fdafd7792e9a117413bcc33167ce9d777
José Sánchez-Gallego <gallegoj@uw.edu>
2021-01-04 09:40:41 -0800

    Add better identifiers for keyword rows

--- src/views/keywords.tsx
+++ src/views/keywords.tsx
@@ -13,6 +13,14 @@ import { ColDef, DataGrid, RowsProp, ValueFormatterParams } from '@material-ui/d
 import { KeywordMap } from '../../electron/tron';
 import { useKeywords } from '../hooks';
 
+function Sep(props: any) {
+  return (
+    <span style={{ color: 'gray', margin: '0px 8px' }} {...props}>
+      |
+    </span>
+  );
+}
+
 const columns: ColDef[] = [
   { field: 'actor', headerName: 'Actor', width: 120, sortDirection: 'asc' },
   { field: 'key', headerName: 'Key', width: 150 },
@@ -22,11 +30,12 @@ const columns: ColDef[] = [
     flex: 10,
     renderCell: (params: ValueFormatterParams) => {
       let values = params.value as unknown[];
-      let sep = <span style={{ color: 'gray', margin: '0px 8px' }}>|</span>;
       let formattedValues: any[] = [];
       for (let idx = 0; idx < values.length; idx++) {
         formattedValues.push(values[idx]);
-        if (idx < values.length - 1) formattedValues.push(sep);
+        if (idx < values.length - 1) {
+          formattedValues.push(<Sep key={idx + 1} />);
+        }
       }
       return <div>{formattedValues}</div>;
     }
@@ -54,16 +63,15 @@ export default function KeywordsView() {
 
   const formatRows = (kws: KeywordMap): RowsProp => {
     let result: RowsProp = [];
-    let id = 1;
     for (let kw in kws) {
+      let kk = kws[kw];
       result.push({
-        id: id,
-        actor: kws[kw].actor,
-        key: kws[kw].key,
-        value: kws[kw].values,
-        lastSeen: kws[kw].lastSeenAt.toISOString().split('T').join(' ')
+        id: `${kk.actor}.${kk.key}`,
+        actor: kk.actor,
+        key: kk.key,
+        value: kk.values,
+        lastSeen: kk.lastSeenAt.toISOString().split('T').join(' ')
       });
-      id++;
     }
     return result;
   };
