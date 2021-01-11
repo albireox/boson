f089fce8a012673c7ac9abcc248613be3f983c2f
José Sánchez-Gallego <gallegoj@uw.edu>
2021-01-11 15:23:24 -0800

    Working command input in log window

--- src/renderer/views/log/input.tsx
+++ src/renderer/views/log/input.tsx
@@ -8,23 +8,74 @@
  *  @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
  */
 
-import { Box, TextField, TextFieldProps } from '@material-ui/core';
-import React from 'react';
+import {
+  Box,
+  IconButton,
+  InputAdornment,
+  makeStyles,
+  OutlinedInput,
+  TextFieldProps
+} from '@material-ui/core';
+import SendIcon from '@material-ui/icons/Send';
+import React, { SyntheticEvent } from 'react';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'row',
+    padding: '4px 8px 8px'
+  },
+  commandInput: {
+    height: '40px'
+  }
+}));
 
 const CommandInput: React.FC<TextFieldProps> = (props) => {
+  const classes = useStyles();
+  const commandRef = React.useRef<any>(null);
+
+  const handleCommand = (event: SyntheticEvent) => {
+    event.preventDefault();
+
+    window.api.invoke(
+      'tron-send-command',
+      commandRef.current.firstChild.value
+    );
+
+    commandRef.current.firstChild.value = '';
+  };
+
   return (
-    <Box>
-      <TextField
-        variant='outlined'
-        margin='none'
-        fullWidth
-        id='command'
-        label=''
-        name='command'
-        autoFocus
-        size='small'
-        style={{ padding: '4px 8px 8px' }}
-      />
+    <Box className={classes.root}>
+      <form
+        onSubmit={handleCommand}
+        autoComplete='off'
+        style={{ width: '100%' }}
+      >
+        <OutlinedInput
+          className={classes.commandInput}
+          ref={commandRef}
+          fullWidth
+          margin='none'
+          id='command'
+          name='command'
+          autoFocus
+          endAdornment={
+            <InputAdornment position='end'>
+              <IconButton
+                color='primary'
+                disableFocusRipple
+                disableRipple
+                size='small'
+                onClick={handleCommand}
+                onMouseDown={handleCommand}
+              >
+                {<SendIcon />}
+              </IconButton>
+            </InputAdornment>
+          }
+        />
+      </form>
     </Box>
   );
 };
