2b27bc05dc74e63f1588d4a3bb6bbd88bcc42da3
José Sánchez-Gallego <gallegoj@uw.edu>
2021-10-11 16:49:05 -0700

    Add button for GotoFieldView

--- src/renderer/views/HAL/goto_field.tsx
+++ src/renderer/views/HAL/goto_field.tsx
@@ -5,13 +5,26 @@
  *  @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
  */
 
-import { Divider, Paper, Stack, Typography } from '@mui/material';
+import { Box, Divider, Paper, Stack, Typography } from '@mui/material';
 import React from 'react';
+import { CommandButton } from 'renderer/components/commandButton';
+import { HALContext } from '.';
 import MacroStepper from './macro_stepper';
 
 /** @jsxImportSource @emotion/react */
 
 export default function GotoFieldView(): JSX.Element | null {
+  const halKeywords = React.useContext(HALContext);
+  const running_macros = halKeywords['hal.running_macros'];
+
+  const [disabled, setDisabled] = React.useState(false);
+
+  React.useEffect(() => {
+    if (running_macros) {
+      setDisabled(running_macros.values.includes('goto_field'));
+    }
+  }, [running_macros]);
+
   return (
     <Paper variant='outlined'>
       <Stack
@@ -24,6 +37,10 @@ export default function GotoFieldView(): JSX.Element | null {
       >
         <Stack alignItems='center' direction='row' spacing={2}>
           <Typography variant='h6'>Goto Field</Typography>
+          <Box flexGrow={1} />
+          <CommandButton disabled={disabled} commandString={'hal goto-field'} size='medium'>
+            Run
+          </CommandButton>
         </Stack>
         <Stack alignItems='center' direction='row' spacing={2} overflow='scroll'>
           <MacroStepper macroName='goto_field' />
