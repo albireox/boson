fe3d804327ddecb275ac938ae7deb6d61ad727d9
José Sánchez-Gallego <gallegoj@uw.edu>
2022-01-17 18:49:21 -0800

    Allow to open image in external DS9

--- src/renderer/views/guider/js9.tsx
+++ src/renderer/views/guider/js9.tsx
@@ -15,26 +15,34 @@ import { useKeywords, useWindowSize } from 'renderer/hooks';
 import { IJS9Opts, JS9Opts } from '.';
 import { MenuBar } from './menubar';
 
-export const JS9: React.FC<{
+type JS9Props = {
   keywords: KeywordMap;
   size: number;
   gid: number;
   opts: IJS9Opts;
   zoomed: number;
+  selected: number;
   setZoomed: (arg0: number) => void;
-}> = ({ keywords, gid, size, opts, zoomed, setZoomed }) => {
+  setSelected: (arg0: number) => void;
+  updateURLs: (arg0: number, arg1: string) => void;
+};
+
+export const JS9 = ({
+  keywords,
+  gid,
+  size,
+  opts,
+  zoomed,
+  selected,
+  setZoomed,
+  setSelected,
+  updateURLs
+}: JS9Props) => {
   const [currentImage, setCurrentImage] = React.useState<string>('');
   const [first, setFirst] = React.useState(true);
 
-  const [hostname, setHostname] = React.useState('');
-  const [port, setPort] = React.useState('');
-
-  window.api.store.get('user.connection.httpHost').then((value) => {
-    setHostname(value);
-  });
-  window.api.store.get('user.connection.httpPort').then((value) => {
-    setPort(value);
-  });
+  const hostname = window.api.store.get_sync('user.connection.httpHost');
+  const port = window.api.store.get_sync('user.connection.httpPort');
 
   let display = `gfa${gid}`;
   const visible = zoomed === 0 || (zoomed && gid === zoomed);
@@ -87,10 +95,12 @@ export const JS9: React.FC<{
     }
 
     setCurrentImage(fullPath);
+    updateURLs(gid, `http://${hostname}:${port}${fullPath}`);
+
     if (first) {
       setFirst(false);
     }
-  }, [keywords, display, currentImage, first, gid, hostname, port]);
+  }, [keywords, display, currentImage, first, gid, hostname, port, updateURLs, zoomed]);
 
   React.useEffect(() => {
     window.JS9.SetColormap(opts.colormap, { display: display });
@@ -107,6 +117,10 @@ export const JS9: React.FC<{
           display: visible ? 'inherit' : 'none'
         }}
         css={(theme: any) => ({
+          borderWidth: '2px',
+          borderStyle: 'solid',
+          borderColor:
+            selected === gid && zoomed !== gid ? 'darkred' : theme.palette.background.default,
           'div.JS9Container > canvas.JS9Image': {
             backgroundColor: theme.palette.background.default,
             backgroundImage: first
@@ -124,6 +138,7 @@ export const JS9: React.FC<{
         data-height={size}
         id={display}
         onDoubleClick={() => setZoomed(gid)}
+        onClick={() => setSelected(gid)}
       >
         <IconButton
           size='large'
@@ -135,7 +150,10 @@ export const JS9: React.FC<{
             visibility: zoomed === gid ? 'visible' : 'hidden',
             zIndex: 10
           }}
-          onClick={() => setZoomed(0)}
+          onClick={() => {
+            setZoomed(0);
+            setSelected(0);
+          }}
         >
           <CancelIcon fontSize='inherit' />
         </IconButton>
@@ -149,28 +167,48 @@ export function JS9Frame() {
   let win_size = useWindowSize();
 
   const [zoomed, setZoomed] = React.useState<number>(0);
+  const [selected, setSelected] = React.useState<number>(0);
+  const [urls, setUrls] = React.useState<{ [key: number]: string }>({});
+
   const [opts, setOpts] = React.useState<IJS9Opts>(JS9Opts);
   const onOptsUpdate = (newOpts: Partial<IJS9Opts>) => {
     setOpts({ ...opts, ...newOpts });
   };
 
-  // window.JS9.globalOpts['mouseActions'][0] = 'none';
-  // window.JS9.globalOpts['resize'] = false;
-  // window.JS9.globalOpts['helperType'] = 'none';
+  window.JS9.globalOpts['resize'] = false;
   window.JS9.globalOpts.alerts = false;
 
   let default_size = Math.round((win_size.height || 800) / 4);
   if (default_size > (win_size.width || 700) / 3) default_size = (win_size.width || 700) / 3;
 
+  const updateSelected = React.useCallback(
+    (gid: number) => {
+      if (gid === 0) {
+        setSelected(0);
+      } else if (gid === selected) {
+        setSelected(0);
+      } else {
+        setSelected(gid);
+      }
+    },
+    [selected]
+  );
+
+  const updateURLs = (gid: number, url: string) => {
+    setUrls((urls) => ({ ...urls, [gid]: url }));
+  };
+
   return (
     <>
       <MenuBar
+        gidSelected={selected || zoomed}
+        urls={urls}
+        onUpdate={onOptsUpdate}
         style={{
           marginBottom: '4px',
           display: 'flex',
           flexDirection: 'row'
         }}
-        onUpdate={onOptsUpdate}
       />
       <Stack width='100%' display='flex' flexDirection='row' justifyContent='center'>
         {[1, 2, 3].map((gid: number) => (
@@ -180,7 +218,10 @@ export function JS9Frame() {
             opts={opts}
             size={zoomed ? 2 * default_size : default_size}
             gid={gid}
+            selected={selected}
             setZoomed={setZoomed}
+            setSelected={updateSelected}
+            updateURLs={updateURLs}
             zoomed={zoomed}
           />
         ))}
@@ -193,7 +234,10 @@ export function JS9Frame() {
             opts={opts}
             size={zoomed ? 2 * default_size : default_size}
             gid={gid}
+            selected={selected}
+            setSelected={updateSelected}
             setZoomed={setZoomed}
+            updateURLs={updateURLs}
             zoomed={zoomed}
           />
         ))}
--- src/renderer/views/guider/menubar.tsx
+++ src/renderer/views/guider/menubar.tsx
@@ -7,15 +7,60 @@
 
 /** @jsxImportSource @emotion/react */
 
-import { Button, ButtonGroup, FormControl, MenuItem, Select, Stack } from '@mui/material';
+import OpenInBrowser from '@mui/icons-material/OpenInBrowser';
+import {
+  Button,
+  ButtonGroup,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  FormControl,
+  MenuItem,
+  Select,
+  Stack,
+  Tooltip
+} from '@mui/material';
 import React from 'react';
 import { IJS9Opts, JS9Opts } from '.';
 
-export const MenuBar: React.FC<
-  React.HTMLProps<HTMLDivElement> & {
-    onUpdate: (newOpts: Partial<IJS9Opts>) => void;
-  }
-> = ({ onUpdate, ...props }) => {
+type MenuBarProps = {
+  onUpdate: (newOpts: Partial<IJS9Opts>) => void;
+  gidSelected: number;
+  urls: { [gid: number]: string };
+};
+
+const AlertDialog = ({
+  open,
+  message,
+  onOK
+}: {
+  open: boolean;
+  message: string;
+  onOK: () => void;
+}) => {
+  return (
+    <Dialog open={open} fullWidth={true} maxWidth='xs'>
+      <DialogTitle>Error</DialogTitle>
+      <DialogContent>
+        <DialogContentText>{message}</DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onOK} autoFocus>
+          OK
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export const MenuBar = ({
+  onUpdate,
+  gidSelected,
+  urls,
+  ...props
+}: MenuBarProps & React.HTMLProps<HTMLDivElement>) => {
   const cmaps = ['Grey', 'Heat', 'Cool', 'Viridis', 'Magma', 'Red', 'Green', 'Blue'];
 
   const scales = [
@@ -34,6 +79,8 @@ export const MenuBar: React.FC<
   const [cmap, setCmap] = React.useState(JS9Opts.colormap);
   const [scale, setScale] = React.useState(JS9Opts.scale);
 
+  const [errorMessage, setErrorMessage] = React.useState<string | undefined>(undefined);
+
   const applyZoom = (command: string) => {
     for (let i = 1; i <= 6; i++) {
       window.JS9.SetZoom(command, { display: `gfa${i}` });
@@ -46,71 +93,113 @@ export const MenuBar: React.FC<
     }
   };
 
+  const openInDS9 = () => {
+    if (gidSelected === 0) {
+      setErrorMessage('You must select or zoom on an image to open it in DS9.');
+      return;
+    }
+
+    if (!urls[gidSelected]) {
+      setErrorMessage('There is no image associated with this JS9 frame.');
+      return;
+    }
+
+    for (const command of ['frame new', `url ${urls[gidSelected]}`]) {
+      window.api
+        .openInApplication(`/Users/gallegoj/.pyenv/shims/xpaset -p ds9 ${command}`)
+        .catch((err: Error) => {
+          setErrorMessage(
+            `${err.message} xpaset must be installed and DS9 open and connected. ` +
+              'Use the preferences to change the path to xpaset.'
+          );
+        });
+    }
+  };
+
   return (
-    <div {...props}>
-      <Stack direction='row' spacing={1} sx={{ alignSelf: 'left' }}>
-        <FormControl size='small'>
-          <Select
-            value={cmap}
-            onChange={(event) => {
-              setCmap(event.target.value as string);
-              onUpdate({ colormap: event.target.value as string });
-            }}
-          >
-            {cmaps.map((_cmap) => {
-              return (
-                <MenuItem key={_cmap} value={_cmap.toLowerCase()}>
-                  <img
-                    alt={_cmap}
-                    style={{ marginRight: '4px' }}
-                    src={
-                      process.env.PUBLIC_URL +
-                      `/js9/images/toolbar/dax_images/cmap_${_cmap.toLowerCase()}.png`
-                    }
-                  />
-                  {_cmap}
-                </MenuItem>
-              );
-            })}
-          </Select>
-        </FormControl>
-        <FormControl size='small'>
-          <Select
-            value={scale}
-            onChange={(event) => {
-              setScale(event.target.value as string);
-              onUpdate({ scale: event.target.value as string });
-            }}
-          >
-            {scales.map((_scale) => {
-              return (
-                <MenuItem key={_scale} value={_scale.toLowerCase()}>
-                  {_scale}
-                </MenuItem>
-              );
-            })}
-          </Select>
-        </FormControl>
-      </Stack>
-      <Stack sx={{ flexGrow: 1 }}></Stack>
-      <Stack direction='row' spacing={1} sx={{ alignSelf: 'center' }}>
-        <FormControl>
-          <Button variant='outlined' size='medium' onClick={(e) => reset()}>
-            Reset
-          </Button>
-        </FormControl>
-        <FormControl>
-          <Button variant='outlined' size='medium' onClick={(e) => applyZoom('toFit')}>
-            Fit
-          </Button>
-        </FormControl>
-        <FormControl>
-          <ButtonGroup variant='outlined'>
-            <Button onClick={(e) => applyZoom('out')}>-</Button>
-            <Button onClick={(e) => applyZoom('in')}>+</Button>
-          </ButtonGroup>
-        </FormControl>
-      </Stack>
-    </div>
+    <>
+      <div {...props}>
+        <Stack direction='row' spacing={1} sx={{ alignSelf: 'left' }}>
+          <FormControl size='small'>
+            <Select
+              value={cmap}
+              onChange={(event) => {
+                setCmap(event.target.value as string);
+                onUpdate({ colormap: event.target.value as string });
+              }}
+            >
+              {cmaps.map((_cmap) => {
+                return (
+                  <MenuItem key={_cmap} value={_cmap.toLowerCase()}>
+                    <img
+                      alt={_cmap}
+                      style={{ marginRight: '4px' }}
+                      src={
+                        process.env.PUBLIC_URL +
+                        `/js9/images/toolbar/dax_images/cmap_${_cmap.toLowerCase()}.png`
+                      }
+                    />
+                    {_cmap}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
+          <FormControl size='small'>
+            <Select
+              value={scale}
+              onChange={(event) => {
+                setScale(event.target.value as string);
+                onUpdate({ scale: event.target.value as string });
+              }}
+            >
+              {scales.map((_scale) => {
+                return (
+                  <MenuItem key={_scale} value={_scale.toLowerCase()}>
+                    {_scale}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
+        </Stack>
+        <Stack sx={{ flexGrow: 1 }}></Stack>
+        <Stack direction='row' spacing={1} sx={{ alignSelf: 'center' }}>
+          <FormControl>
+            <Tooltip title='Open in DS9'>
+              <Button
+                sx={{ height: '33px', minWidth: '0px' }}
+                variant='outlined'
+                size='medium'
+                onClick={(e) => openInDS9()}
+              >
+                <OpenInBrowser />
+              </Button>
+            </Tooltip>
+          </FormControl>
+          <FormControl>
+            <Button variant='outlined' size='medium' onClick={(e) => reset()}>
+              Reset
+            </Button>
+          </FormControl>
+          <FormControl>
+            <Button variant='outlined' size='medium' onClick={(e) => applyZoom('toFit')}>
+              Fit
+            </Button>
+          </FormControl>
+          <FormControl>
+            <ButtonGroup variant='outlined'>
+              <Button onClick={(e) => applyZoom('out')}>-</Button>
+              <Button onClick={(e) => applyZoom('in')}>+</Button>
+            </ButtonGroup>
+          </FormControl>
+        </Stack>
+      </div>
+      <AlertDialog
+        open={errorMessage !== undefined}
+        message={errorMessage || ''}
+        onOK={() => setErrorMessage(undefined)}
+      />
+    </>
   );
 };
