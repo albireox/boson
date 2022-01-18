cca6d4b2e27da04a84c20266f7466d2105f2c568
José Sánchez-Gallego <gallegoj@uw.edu>
2022-01-17 18:47:54 -0800

    Disable attempts to use JS9 helper

--- public/js9prefs.js
+++ public/js9prefs.js
@@ -1,19 +1,17 @@
 var JS9Prefs = {
   globalOpts: {
-    helperType: 'sock.io',
+    helperType: 'none',
     helperPort: 2718,
     helperCGI: './cgi-bin/js9/js9Helper.cgi',
-    debug: 1,
+    debug: 0,
     loadProxy: false,
     workDir: './tmp',
     workDirQuota: 100,
     dataPath: '$HOME/Desktop:$HOME/Downloads:/data/gcam',
     analysisPlugins: './analysis-plugins',
     analysisWrappers: './analysis-wrappers',
-    resize: true,
-    fits2fits: 'always',
-    image: { xdim: 2048, ydim: 2048, bin: 4 },
-    table: { xdim: 2048, ydim: 2048, bin: 4 },
+    resize: false,
+    fits2fits: 'never',
     alerts: false,
     waitType: 'mouse'
   },
