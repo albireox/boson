27137a6a4995f5041a3f3bdb1c8317b705b958b5
José Sánchez-Gallego <gallegoj@uw.edu>
2022-01-19 21:40:25 -0800

    Add search+highlight capabilities in snapshot and use theme provider to set light theme

--- src/renderer/views/snapshots.tsx
+++ src/renderer/views/snapshots.tsx
@@ -5,25 +5,32 @@
  *  @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
  */
 
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import { ThemeProvider } from '@emotion/react';
+import CloseIcon from '@mui/icons-material/Close';
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import OpenInBrowserIcon from '@mui/icons-material/OpenInBrowser';
-import { Button, ButtonGroup, Stack, Typography } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import {
+  Button,
+  ButtonGroup,
+  Collapse,
+  createTheme,
+  IconButton,
+  InputAdornment,
+  OutlinedInput,
+  Stack,
+  Typography
+} from '@mui/material';
 import { blue, grey } from '@mui/material/colors';
-import { styled } from '@mui/system';
 import React from 'react';
-import { Document, Page } from 'react-pdf';
+import { Document, Page, TextLayerItemInternal } from 'react-pdf';
 import { useKeywords, useWindowSize } from 'renderer/hooks';
+
 let host: string = window.api.store.get_sync('user.connection.httpHost');
 let port: number = window.api.store.get_sync('user.connection.httpPort');
 
 if (~host.startsWith('http')) host = 'http://' + host;
-
-const ButtonBlue = styled(Button)(({ theme }) => ({
-  backgroundColor: blue[700],
-  color: 'white',
-  '&.Mui-disabled': { backgroundColor: grey[500], color: grey[300] }
-}));
 
 export default function SnapshotsView() {
   const keywords = useKeywords(['jaeger.configuration_snapshot', 'jaeger.snapshot'], 'snapshots');
@@ -35,7 +42,47 @@ export default function SnapshotsView() {
   const [disabledPrevious, setDisabledPrevious] = React.useState(true);
   const [disabledNext, setDisabledNext] = React.useState(true);
 
+  const [showSearch, setShowSearch] = React.useState(false);
+  const [searchText, setSearchText] = React.useState('');
+  const [searchTextShow, setSearchTextShow] = React.useState('');
+
+  const renderHighlight = React.useCallback(
+    (layer: TextLayerItemInternal) => {
+      if (showSearch) {
+        if (searchText.length >= 3 && layer.str.includes(searchText)) {
+          const size: number = 30 * layer.scale!;
+          console.log(layer);
+          return (
+            <div
+              style={{
+                borderRadius: '50%',
+                height: `${size}px`,
+                width: `${size}px`,
+                border: '2px solid darkred',
+                position: 'absolute',
+                left: `-${size / 2}px`,
+                top: `-${size / 2}px`
+              }}
+            />
+          );
+        }
+      }
+
+      return <span />;
+    },
+    [showSearch, searchText]
+  );
+
   let win_size = useWindowSize();
+
+  const lightTheme = createTheme({
+    palette: {
+      mode: 'light'
+    },
+    typography: {
+      fontSize: 12
+    }
+  });
 
   React.useEffect(() => {
     for (const kk of ['jaeger.configuration_snapshot', 'jaeger.snapshot']) {
@@ -56,10 +103,10 @@ export default function SnapshotsView() {
 
   const increaseScale = () => {
     setScale((s) => {
-      if (s >= 4.0) {
-        return 4.0;
+      if (s >= 6.0) {
+        return 6.0;
       } else {
-        return s + 0.5;
+        return s + 1;
       }
     });
   };
@@ -69,7 +116,7 @@ export default function SnapshotsView() {
       if (s === 1) {
         return 1;
       } else {
-        return s - 0.5;
+        return s - 1;
       }
     });
   };
@@ -93,68 +140,114 @@ export default function SnapshotsView() {
   };
 
   return (
-    <div
-      style={{
-        overflow: scale === 1 ? 'hidden' : 'scroll',
-        backgroundColor: 'white',
-        height: win_size.height
-      }}
-    >
-      <Stack
-        direction='row'
-        spacing={1}
-        sx={{ zIndex: 1, position: 'fixed', width: '100%' }}
-        padding={1}
+    <ThemeProvider theme={lightTheme}>
+      <div
+        style={{
+          overflow: scale === 1 ? 'hidden' : 'scroll',
+          backgroundColor: 'white',
+          height: win_size.height
+        }}
       >
-        <Stack direction='row' spacing={1} sx={{ alignSelf: 'left' }}>
-          <ButtonGroup variant='contained'>
-            <ButtonBlue
-              onClick={() => setIndex((idx) => (idx === 0 ? 0 : idx - 1))}
-              sx={{ padding: 0 }}
-              disabled={disabledPrevious}
-            >
-              <ArrowBackIosIcon />
-            </ButtonBlue>
-            <ButtonBlue
-              onClick={() => setIndex((idx) => (idx === snapshots.length - 1 ? idx : idx + 1))}
-              sx={{ padding: 0 }}
-              disabled={disabledNext}
-            >
-              <ArrowForwardIosIcon />
-            </ButtonBlue>
-          </ButtonGroup>
+        <Stack
+          direction='row'
+          spacing={1}
+          sx={{ zIndex: 1, position: 'fixed', width: '100%' }}
+          padding={1}
+        >
+          <Stack direction='row' spacing={1} sx={{ alignSelf: 'left' }}>
+            <ButtonGroup variant='contained' size='small'>
+              <Button
+                onClick={() => setIndex((idx) => (idx === 0 ? 0 : idx - 1))}
+                sx={{ padding: 0 }}
+                disabled={disabledPrevious}
+              >
+                <NavigateBeforeIcon />
+              </Button>
+              <Button
+                onClick={() => setIndex((idx) => (idx === snapshots.length - 1 ? idx : idx + 1))}
+                sx={{ padding: 0 }}
+                disabled={disabledNext}
+              >
+                <NavigateNextIcon />
+              </Button>
+            </ButtonGroup>
 
-          <ButtonBlue
-            variant='contained'
-            size='medium'
-            endIcon={<OpenInBrowserIcon />}
-            onClick={openInBrowser}
-          >
-            Open in browser
-          </ButtonBlue>
+            <Button
+              variant='contained'
+              size='medium'
+              endIcon={<OpenInBrowserIcon />}
+              onClick={openInBrowser}
+            >
+              Open in browser
+            </Button>
+          </Stack>
+          <Stack sx={{ flexGrow: 1, alignItems: 'center', alignSelf: 'center' }}>
+            <Typography variant='h5' color={grey[800]}>
+              {snapshots[index] ? snapshots[index].split('/').reverse()[0] : ''}
+            </Typography>
+          </Stack>
+          <Stack direction='row' spacing={1} sx={{ alignSelf: 'right' }}>
+            <Button variant='contained' size='medium' onClick={() => setScale(1)}>
+              Fit
+            </Button>
+            <ButtonGroup variant='contained'>
+              <Button onClick={decreaseScale}>-</Button>
+              <Button onClick={increaseScale}>+</Button>
+            </ButtonGroup>
+          </Stack>
+          <IconButton size='small' onClick={() => setShowSearch((s) => !s)}>
+            <SearchIcon sx={{ color: blue[700] }} fontSize='medium' />
+          </IconButton>
+          <Collapse in={showSearch}>
+            <OutlinedInput
+              color='secondary'
+              size='small'
+              value={searchTextShow}
+              onChange={(e) => {
+                setSearchTextShow(e.target.value);
+                if (e.target.value.length >= 3) {
+                  setSearchText(e.target.value);
+                }
+              }}
+              sx={(theme) => ({
+                backgroundColor: 'white',
+                boxShadow: '2px 2px 4px black',
+                width: '100px',
+                position: 'absolute',
+                top: '45px',
+                right: '15px',
+                pr: 1
+              })}
+              endAdornment={
+                <InputAdornment position='end'>
+                  <IconButton
+                    size='small'
+                    onClick={() => {
+                      setSearchText('');
+                      setSearchTextShow('');
+                      setShowSearch(false);
+                    }}
+                  >
+                    <CloseIcon />
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </Collapse>
         </Stack>
-        <Stack sx={{ flexGrow: 1, alignItems: 'center', alignSelf: 'center' }}>
-          <Typography variant='h5' color={grey[800]}>
-            {snapshots[index] ? snapshots[index].split('/').reverse()[0] : ''}
-          </Typography>
-        </Stack>
-        <Stack direction='row' spacing={1} sx={{ alignSelf: 'right' }}>
-          <ButtonBlue variant='contained' size='medium' onClick={() => setScale(1)}>
-            Fit
-          </ButtonBlue>
-          <ButtonGroup variant='contained'>
-            <ButtonBlue onClick={decreaseScale}>-</ButtonBlue>
-            <ButtonBlue onClick={increaseScale}>+</ButtonBlue>
-          </ButtonGroup>
-        </Stack>
-      </Stack>
-      <Document
-        file={`${host}:${port}/${snapshots[index]}`}
-        renderMode='canvas'
-        onLoadProgress={updateButtons}
-      >
-        <Page pageNumber={1} scale={scale} width={win_size.width} />
-      </Document>
-    </div>
+        <Document
+          file={`${host}:${port}/${snapshots[index]}`}
+          renderMode='canvas'
+          onLoadProgress={updateButtons}
+        >
+          <Page
+            pageNumber={1}
+            scale={scale}
+            width={win_size.width}
+            customTextRenderer={renderHighlight}
+          />
+        </Document>
+      </div>
+    </ThemeProvider>
   );
 }
