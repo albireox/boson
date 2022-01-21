806d47f5c21debe0d432d1e41973bcd60697d759
José Sánchez-Gallego <gallegoj@uw.edu>
2022-01-20 21:16:25 -0800

    Functional guider table

--- src/renderer/views/guider/guideTable.tsx
+++ src/renderer/views/guider/guideTable.tsx
@@ -10,6 +10,7 @@ import { IconButton, OutlinedInput } from '@mui/material';
 import { Box } from '@mui/system';
 import { DataGridPro, GridColDef, LicenseInfo } from '@mui/x-data-grid-pro';
 import React from 'react';
+import { useKeywords } from 'renderer/hooks';
 
 /** @jsxImportSource @emotion/react */
 
@@ -18,14 +19,72 @@ LicenseInfo.setLicenseKey(
 );
 
 const AxisOffsetInput = (params: { axis: string }) => {
-  const { axis } = params;
+  let { axis } = params;
+
+  const [offset, setOffset] = React.useState('');
+  const [error, setError] = React.useState(false);
+  const [disabled, setDisabled] = React.useState(false);
+
+  axis = axis.toLowerCase();
+
+  if (['scale', 'focus'].includes(axis)) return <div />;
+
+  const handleClick = () => {
+    if (offset === '') return;
+
+    if (Number.isNaN(Number(offset))) {
+      setError(true);
+      return;
+    }
+
+    const offsetDeg = Number(offset) / 3600;
+
+    let command: string;
+
+    switch (axis) {
+      case 'ra':
+        command = `tcc offset arc ${offsetDeg}, 0.0 /computed`;
+        break;
+      case 'declination':
+        command = `tcc offset arc 0.0, ${offsetDeg} /computed`;
+        break;
+      case 'rotator':
+        command = `tcc offset guide 0.0, 0.0, ${offsetDeg} /computed`;
+        break;
+      default:
+        return;
+    }
+
+    setDisabled(true);
+    window.api.tron
+      .send(command)
+      .catch(() => setError(true))
+      .finally(() => {
+        setDisabled(false);
+        setOffset('');
+      });
+  };
 
   return (
     <OutlinedInput
+      error={error}
+      disabled={disabled}
+      value={offset}
       size='small'
       sx={{ minWidth: '150px', pr: '5px' }}
+      onChange={(e) => {
+        setOffset(e.target.value);
+        setError(false);
+      }}
+      onKeyDown={(e) => e.key === 'Enter' && handleClick()}
       endAdornment={
-        <IconButton size='small' sx={{ opacity: 0.3 }} disableFocusRipple>
+        <IconButton
+          disabled={disabled}
+          size='small'
+          sx={{ opacity: 0.5 }}
+          disableFocusRipple
+          onClick={handleClick}
+        >
           <Send fontSize='small' />
         </IconButton>
       }
@@ -34,6 +93,30 @@ const AxisOffsetInput = (params: { axis: string }) => {
 };
 
 export const GuideTable = () => {
+  const keywords = useKeywords([
+    'cherno.pid_radec',
+    'cherno.pid_rot',
+    'cherno.correction_applied',
+    'cherno.did_correct',
+    'cherno.acquisition_valid',
+    'cherno.astrometry_fit'
+  ]);
+
+  const [pid, setPid] = React.useState<number[]>([0.0, 0.0]);
+  const [measured, setMeasured] = React.useState<number[]>([0.0, 0.0, 0.0, 0.0]);
+  const [applied, setApplied] = React.useState<number[]>([0.0, 0.0, 0.0]);
+
+  React.useEffect(() => {
+    keywords['cherno.pid_radec'] && setPid((d) => [keywords['cherno.pid_radec'].values[0], d[1]]);
+    keywords['cherno.pid_rot'] && setPid((d) => [d[0], keywords['cherno.pid_rot'].values[0]]);
+
+    const astrometry_fit = keywords['cherno.astrometry_fit'];
+    if (astrometry_fit) setMeasured(astrometry_fit.values.slice(-4));
+
+    const correction_applied = keywords['cherno.correction_applied'];
+    if (correction_applied) setApplied(correction_applied.values);
+  }, [keywords]);
+
   const columns: GridColDef[] = [
     {
       field: 'axis',
@@ -44,22 +127,26 @@ export const GuideTable = () => {
       field: 'measured',
       headerName: 'Measured',
       description: 'Raw value measured from the guider fit',
-      type: 'number',
-      flex: 0.2
+      flex: 0.2,
+      align: 'right',
+      headerAlign: 'right'
     },
     {
       field: 'applied',
       headerName: 'Applied',
       description: 'Correction applied in the last iteration, including the PID scaling',
-      type: 'number',
-      flex: 0.2
+      align: 'right',
+      flex: 0.2,
+      headerAlign: 'right'
     },
     {
       field: 'pid',
       headerName: 'PID',
       description: 'PID parameters for the axis [Kp]',
       sortable: false,
-      flex: 0.1
+      align: 'right',
+      flex: 0.1,
+      headerAlign: 'right'
     },
     {
       field: 'offset',
@@ -76,11 +163,11 @@ export const GuideTable = () => {
   ];
 
   const rows = [
-    { id: 1, axis: 'RA', measured: 0.0, applied: 0.0 },
-    { id: 2, axis: 'Declination', measured: 0.0, applied: 0.0 },
-    { id: 3, axis: 'Rotator', measured: 0.0, applied: 0.0 },
-    { id: 4, axis: 'Scale', measured: 0.0, applied: '' },
-    { id: 5, axis: 'Focus', measured: 0.0, applied: 0.0 }
+    { id: 1, axis: 'RA', measured: measured[0], applied: applied[0], pid: pid[0] },
+    { id: 2, axis: 'Declination', measured: measured[1], applied: applied[1], pid: pid[0] },
+    { id: 3, axis: 'Rotator', measured: measured[2], applied: applied[2], pid: pid[1] },
+    { id: 4, axis: 'Scale', measured: measured[3], applied: '' },
+    { id: 5, axis: 'Focus', measured: '', applied: '' }
   ];
   return (
     <Box width='100%' py={2}>
