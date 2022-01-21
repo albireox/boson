a71fd8364e111e05f301389354a7491580543448
José Sánchez-Gallego <gallegoj@uw.edu>
2022-01-20 22:25:00 -0800

    Use secondary color when axis is disabled

--- src/renderer/views/guider/guide.tsx
+++ src/renderer/views/guider/guide.tsx
@@ -48,10 +48,28 @@ const AxesGroup = () => {
   };
 
   return (
-    <ToggleButtonGroup color='primary' value={enabledAxes} onChange={handleChange} size='small'>
-      <ToggleButton value='radec'>RA|Dec</ToggleButton>
-      <ToggleButton value='rot'>Rot</ToggleButton>
-      <ToggleButton value='focus'>Focus</ToggleButton>
+    <ToggleButtonGroup color='secondary' value={enabledAxes} onChange={handleChange} size='small'>
+      <ToggleButton
+        color={enabledAxes.includes('radec') ? 'primary' : 'secondary'}
+        selected={true}
+        value='radec'
+      >
+        RA|Dec
+      </ToggleButton>
+      <ToggleButton
+        color={enabledAxes.includes('rot') ? 'primary' : 'secondary'}
+        selected={true}
+        value='rot'
+      >
+        Rot
+      </ToggleButton>
+      <ToggleButton
+        color={enabledAxes.includes('focus') ? 'primary' : 'secondary'}
+        selected={true}
+        value='focus'
+      >
+        Focus
+      </ToggleButton>
     </ToggleButtonGroup>
   );
 };
