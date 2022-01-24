20b5d501764328bd00ddd69fd5e5f25d5664e7f8
José Sánchez-Gallego <gallegoj@uw.edu>
2022-01-24 15:29:33 -0800

    Move BorderLinearProgress component to its own file

--- src/renderer/views/HAL/scripts.tsx
+++ src/renderer/views/HAL/scripts.tsx
@@ -6,24 +6,10 @@
  */
 
 import { Box, MenuItem, Paper, Select, Stack, Typography } from '@mui/material';
-import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
-/** @jsxImportSource @emotion/react */
-import { styled } from '@mui/material/styles';
 import { CommandStatus, KeywordMap, ReplyCode } from 'main/tron';
 import React from 'react';
+import { BorderLinearProgress } from 'renderer/components/borderLinealProgress';
 import { CommandButton } from 'renderer/components/commandButton';
-
-const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
-  height: 10,
-  borderRadius: 5,
-  [`&.${linearProgressClasses.colorPrimary}`]: {
-    backgroundColor: theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800]
-  },
-  [`& .${linearProgressClasses.bar}`]: {
-    borderRadius: 5,
-    backgroundColor: theme.palette.mode === 'light' ? '#1a90ff' : '#308fe8'
-  }
-}));
 
 interface IHALScripts {
   keywords: KeywordMap;
