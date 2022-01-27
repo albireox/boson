570ef8b7b1e421f8fa83196e29730382c9782048
José Sánchez-Gallego <gallegoj@uw.edu>
2022-01-26 19:26:44 -0800

    Increase cadence of messages in log window

--- src/renderer/views/log/message.tsx
+++ src/renderer/views/log/message.tsx
@@ -269,7 +269,7 @@ const Messages: React.FC<MessagesProps> = ({ onConfigUpdate }) => {
         search: search
       });
       setBuffer([]);
-    }, 500);
+    }, 100);
     return () => clearInterval(timer);
   }, [buffer, config, search, dispatch]);
 
