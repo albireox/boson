7c019dab0e0620a66d524c079c6af49be429244d
José Sánchez-Gallego <gallegoj@uw.edu>
2022-01-29 14:26:14 -0800

    Update deps

--- scripts/copy-deps.js
+++ scripts/copy-deps.js
@@ -1,12 +1,8 @@
 const fs = require('fs-extra');
 const path = require('path');
 
-const pdfjsDistPath = path.join(
-  path.dirname(require.resolve('react-pdf/package.json')),
-  'node_modules',
-  'pdfjs-dist'
-);
-const pdfWorkerPath = path.join(pdfjsDistPath, 'legacy', 'build', 'pdf.worker.js');
+const pdfjsDistPath = path.dirname(require.resolve('pdfjs-dist/package.json'));
+const pdfWorkerPath = path.join(pdfjsDistPath, 'build', 'pdf.worker.js');
 
 const targetDir = 'public';
 const targetPath = path.join(targetDir, 'pdf.worker.js');
