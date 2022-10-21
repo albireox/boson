e7514710d4e5087e8a76f793ebc842b959023191
José Sánchez-Gallego <gallegoj@uw.edu>
2022-10-20 18:07:22 -0700

    Remove public/js9 before copying a new version

--- scripts/copy-deps.js
+++ scripts/copy-deps.js
@@ -4,4 +4,7 @@ const path = require('path');
 const targetDir = 'public';
 
 // Copy js9
+if (fs.pathExistsSync(path.join(targetDir, 'js9'))) {
+  fs.rmdirSync(path.join(targetDir, 'js9'), { recursive: true, force: true });
+}
 fs.copy(path.dirname(require.resolve('js9/package.json')), path.join(targetDir, 'js9'));
