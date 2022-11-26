319e49e5ce4ba09218e53cdc0a4f3fdd827c7a29
José Sánchez-Gallego <gallegoj@uw.edu>
2022-11-25 19:25:46 -0800

    Add more info to MainStatus and fix elapsed time

--- src/renderer/Main/MainStatus.tsx
+++ src/renderer/Main/MainStatus.tsx
@@ -25,6 +25,7 @@ const StatusText = ({ color = undefined, children }: StatusTextProps) => {
       component='span'
       fontSize={16}
       color={color}
+      sx={{ WebkitUserSelect: 'unset' }}
     >
       {children}
     </Typography>
@@ -48,21 +49,67 @@ const MainStatus = () => {
     updateInterval: 1,
   });
 
-  const formatElapsedTime = () => {
-    const date = new Date(0);
-    date.setSeconds(elapsedTime);
-    return date.toISOString().substring(11, 19);
-  };
+  const formatElapsedTime = React.useCallback((elap_: number) => {
+    let elap = elap_;
 
-  const [program] = useStore<string>('connection.program', true);
-  const [host] = useStore<string>('connection.host', true);
-  const [port] = useStore<number>('connection.port', true);
+    const nDays = Math.trunc(elap / (24 * 3600));
+    if (nDays > 0) elap -= nDays * 24 * 3600;
+
+    const date = new Date(0);
+    date.setSeconds(elap);
+
+    let prefix = '';
+    if (nDays === 1) {
+      prefix = '1 day ';
+    } else if (nDays > 1) {
+      prefix = `${nDays} days `;
+    }
+
+    return prefix + date.toISOString().substring(11, 19);
+  }, []);
+
+  const [program, setProgram] = useState('N/A');
+  const [host, setHost] = useState('N/A');
+  const [port, setPort] = useState('N/A');
+  const [user, setUser] = useState('N/A');
+
   const [needsAuthentication] = useStore<number>(
-    'connection.needsAuthentication',
-    true
+    'connection.needsAuthentication'
   );
 
+  const hostPort = host !== 'N/A' ? `${host}:${port}` : 'N/A';
+
   useEffect(() => {
+    const startTimer = () => {
+      window.electron.tron
+        .getLastConnected()
+        .then((value: Date) => {
+          return resetElapsed((Date.now() - value.getTime()) / 1000);
+        })
+        .catch(() => {});
+      setIsPlaying(true);
+    };
+
+    const setCredentials = () => {
+      window.electron.tron
+        .getCredentials()
+        .then((credentials) => {
+          setUser(credentials[1]);
+          setProgram(credentials[2]);
+          setHost(credentials[3]);
+          setPort(credentials[4].toString());
+          return true;
+        })
+        .catch(() => {});
+    };
+
+    const resetCredentials = () => {
+      setUser('N/A');
+      setProgram('N/A');
+      setHost('N/A');
+      setPort('N/A');
+    };
+
     window.electron.app
       .getVersion()
       .then((result) => setVersion(result))
@@ -80,16 +127,13 @@ const MainStatus = () => {
             Authorised
           </StatusText>
         );
-        window.electron.tron
-          .getLastConnected()
-          .then((value: Date) => {
-            return resetElapsed((Date.now() - value.getTime()) / 1000);
-          })
-          .catch(() => {});
-        setIsPlaying(true);
+        startTimer();
+        setCredentials();
       } else {
         resetElapsed();
         setIsPlaying(false);
+        resetCredentials();
+
         if (ConnectionStatus.Connected & status) {
           if (needsAuthentication) {
             setConnectionText(
@@ -103,6 +147,8 @@ const MainStatus = () => {
                 Connected
               </StatusText>
             );
+            startTimer();
+            setCredentials();
           }
         } else if (ConnectionStatus.Connecting & status) {
           setConnectionText(
@@ -168,11 +214,13 @@ const MainStatus = () => {
               <StatusText>Status: </StatusText>
               {connectionText}
               <br />
-              <StatusText>Elapsed: {formatElapsedTime()}</StatusText>
+              <StatusText>User: {user}</StatusText>
               <br />
               <StatusText>Program: {program.toUpperCase()}</StatusText>
               <br />
-              <StatusText>Host: {`${host}:${port}`}</StatusText>
+              <StatusText>Host: {hostPort}</StatusText>
+              <br />
+              <StatusText>Elapsed: {formatElapsedTime(elapsedTime)}</StatusText>
             </CardContent>
           </Card>
         </Box>
