e2cfe511ebc0f956b1b8d0fc318239f330a01a49
José Sánchez-Gallego <gallegoj@uw.edu>
2022-11-25 20:05:08 -0800

    Reorganise code for Main drawer

---
+++ src/renderer/Main/Drawer.tsx
@@ -0,0 +1,102 @@
+/*
+ *  @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+ *  @Date: 2022-11-25
+ *  @Filename: Drawer.tsx
+ *  @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+ */
+
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import MenuIcon from '@mui/icons-material/Menu';
+import NotesIcon from '@mui/icons-material/Notes';
+import SettingsIcon from '@mui/icons-material/Settings';
+import {
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  Stack,
+} from '@mui/material';
+import React from 'react';
+import { DrawerListItem, PersistentDrawer } from 'renderer/Components';
+
+interface FooterProps {
+  open: boolean;
+  setOpen: (mode: boolean) => void;
+  openNewWindow: (name: string) => void;
+}
+
+function Footer(props: FooterProps) {
+  const { open, setOpen, openNewWindow } = props;
+
+  return (
+    <List>
+      <DrawerListItem
+        name='preferences'
+        icon={<SettingsIcon />}
+        text='Preferences'
+        onClick={openNewWindow}
+        open={open}
+      />
+
+      <ListItem
+        disablePadding
+        sx={{ display: 'block' }}
+        onClick={() => setOpen(!open)}
+      >
+        <ListItemButton
+          sx={{
+            minHeight: 48,
+            px: 2.5,
+            '&.MuiButtonBase-root:hover': {
+              bgcolor: 'transparent',
+            },
+          }}
+        >
+          <ListItemIcon
+            sx={{
+              minWidth: 0,
+              mr: open ? 3 : 'auto',
+            }}
+          >
+            {!open ? <MenuIcon /> : <KeyboardArrowLeftIcon />}
+          </ListItemIcon>
+        </ListItemButton>
+      </ListItem>
+    </List>
+  );
+}
+
+export default function Drawer() {
+  const [open, setOpen] = React.useState(false);
+
+  const openNewWindow = (name: string) => {
+    window.electron.app.openNewWindow(name);
+  };
+
+  const Item = (props: {
+    name: string;
+    icon: React.ReactElement;
+    text?: string;
+  }) => {
+    const { name, icon, text } = props;
+    return (
+      <DrawerListItem
+        name={name}
+        icon={icon}
+        text={text ?? ''}
+        onClick={openNewWindow}
+        open={open}
+      />
+    );
+  };
+
+  return (
+    <PersistentDrawer open={open}>
+      <Stack direction='column' height='100%'>
+        <Item name='log' icon={<NotesIcon />} text='New log window' />
+        <div style={{ flexGrow: 1 }} />
+        <Footer open={open} setOpen={setOpen} openNewWindow={openNewWindow} />
+      </Stack>
+    </PersistentDrawer>
+  );
+}
--- src/renderer/Main/Main.tsx
+++ src/renderer/Main/Main.tsx
@@ -5,10 +5,6 @@
  *  @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
  */
 
-import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
-import MenuIcon from '@mui/icons-material/Menu';
-import NotesIcon from '@mui/icons-material/Notes';
-import SettingsIcon from '@mui/icons-material/Settings';
 import {
   Alert,
   Button,
@@ -22,15 +18,10 @@ import {
 } from '@mui/material';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
 import * as React from 'react';
 import { useStore } from 'renderer/hooks';
 import { ConnectionStatus } from '../../main/tron/types';
-import DrawerListItem from '../Components/DrawerListItem';
-import PersistentDrawer from '../Components/PersistentDrawer';
+import Drawer from './Drawer';
 import MainStatus from './MainStatus';
 
 type ConnectStackbarProps = {
@@ -95,8 +86,6 @@ const AuthenticationFailedSnackbar = ({
 };
 
 export default function Main() {
-  const [open, setOpen] = React.useState(false);
-
   const [isConnected, setIsConnected] = React.useState(false);
   const [isConnecting, setIsConnecting] = React.useState(false);
 
@@ -202,60 +191,11 @@ export default function Main() {
     }
   };
 
-  const openNewWindow = (name: string) => {
-    window.electron.app.openNewWindow(name);
-  };
-
   return (
     <>
       <Box sx={{ display: 'flex', height: '100%' }}>
         <CssBaseline />
-        <PersistentDrawer open={open}>
-          <>
-            <DrawerListItem
-              name='log'
-              icon={<NotesIcon />}
-              text='New log window'
-              onClick={openNewWindow}
-              open={open}
-            />
-            <div style={{ flexGrow: 1 }} />
-            <List>
-              <DrawerListItem
-                name='preferences'
-                icon={<SettingsIcon />}
-                text='Preferences'
-                onClick={openNewWindow}
-                open={open}
-              />
-
-              <ListItem
-                disablePadding
-                sx={{ display: 'block' }}
-                onClick={() => setOpen(!open)}
-              >
-                <ListItemButton
-                  sx={{
-                    minHeight: 48,
-                    px: 2.5,
-                    '&.MuiButtonBase-root:hover': {
-                      bgcolor: 'transparent',
-                    },
-                  }}
-                >
-                  <ListItemIcon
-                    sx={{
-                      minWidth: 0,
-                      mr: open ? 3 : 'auto',
-                    }}
-                  >
-                    {!open ? <MenuIcon /> : <KeyboardArrowLeftIcon />}
-                  </ListItemIcon>
-                </ListItemButton>
-              </ListItem>
-            </List>
-          </>
-        </PersistentDrawer>
+        <Drawer />
         <Box sx={{ flexGrow: 1, p: 3, height: '100%' }}>
           <MainStatus />
         </Box>
