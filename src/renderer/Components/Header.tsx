f26c3501b383e6dcb3a29be79ea6158018a0c972
José Sánchez-Gallego <gallegoj@uw.edu>
2022-11-27 16:15:41 -0800

    Use Paper for Header

--- src/renderer/Components/Header.tsx
+++ src/renderer/Components/Header.tsx
@@ -5,8 +5,7 @@
  *  @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
  */
 
-import { Box, Stack } from '@mui/material';
-import { HeaderDivider } from 'renderer/Components';
+import { Paper, Stack } from '@mui/material';
 
 interface HeaderProps {
   children?: React.ReactNode;
@@ -15,27 +14,31 @@ interface HeaderProps {
 export default function Header(props: HeaderProps) {
   const { children } = props;
   return (
-    <Box
-      height='50px'
-      minHeight='50px'
-      position='fixed'
-      top={0}
-      width='100%'
-      bgcolor='background.default'
-      zIndex={1000}
-      sx={{ WebkitAppRegion: 'drag' }}
+    <Paper
+      sx={{
+        WebkitAppRegion: 'drag',
+        height: '50px',
+        minHeight: '50px',
+        position: 'fixed',
+        top: 0,
+        width: '100%',
+        bgcolor: 'background.default',
+        borderRadius: 0,
+        zIndex: 1000,
+      }}
+      elevation={1}
     >
       <Stack
         direction='row'
         alignItems='center'
         height='100%'
+        width='100%'
         spacing={2}
-        pl={12}
+        pl={10}
         pr={2}
       >
         {children}
       </Stack>
-      <HeaderDivider />
-    </Box>
+    </Paper>
   );
 }
