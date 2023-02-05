82729e1f94c2a86a806ee35ce43d231aa7e65f56
José Sánchez-Gallego <gallegoj@uw.edu>
2023-02-04 16:21:42 -0800

    APO-33: show SnackAlert if loading designs while exposing

--- src/renderer/HAL/Header/PreloadedBanner.tsx
+++ src/renderer/HAL/Header/PreloadedBanner.tsx
@@ -9,7 +9,12 @@ import ClearIcon from '@mui/icons-material/Clear';
 import { Button, Collapse, IconButton, Stack, Typography } from '@mui/material';
 import React from 'react';
 import BosonHeader from 'renderer/Components/BosonHeader';
-import { useKeywordContext } from 'renderer/hooks';
+import { useIsExposing, useKeywordContext } from 'renderer/hooks';
+import SnackAlert, { SnackAlertRefType } from '../Components/SnackAlert';
+
+function loadDesign() {
+  window.electron.tron.send('jaeger configuration load --from-preloaded');
+}
 
 export function PreloadedBanner() {
   const {
@@ -19,6 +24,15 @@ export function PreloadedBanner() {
 
   const [preloadedDesignID, setPreloadedDesignID] = React.useState<number>(0);
   const [isCloned, setIsCloned] = React.useState(false);
+
+  const exposing = useIsExposing();
+
+  const preloadAlertRef = React.useRef<SnackAlertRefType>(null);
+
+  React.useEffect(() => {
+    const timeout = setTimeout(() => preloadAlertRef.current?.open(), 3000);
+    return () => clearTimeout(timeout);
+  }, []);
 
   React.useEffect(() => {
     if (!designPreloadedKw) return;
@@ -30,56 +44,68 @@ export function PreloadedBanner() {
     setIsCloned(preloadedIsClonedKw.values[0]);
   }, [preloadedIsClonedKw]);
 
-  const loadDesign = React.useCallback(() => {
-    window.electron.tron.send('jaeger configuration load --from-preloaded');
-  }, []);
+  const checkAndLoad = React.useCallback(() => {
+    if (exposing) {
+      preloadAlertRef.current?.open();
+      return;
+    }
+    loadDesign();
+  }, [exposing]);
 
   const clearDesign = React.useCallback(() => {
     window.electron.tron.send('jaeger configuration preload --clear');
   }, []);
 
   return (
-    <Collapse in={preloadedDesignID > 0}>
-      <BosonHeader>
-        <Stack
-          direction='row'
-          width='100%'
-          sx={{ opacity: preloadedDesignID > 0 ? 1 : 0 }}
-        >
-          <Typography
-            fontSize={16}
-            fontWeight={400}
-            textAlign='center'
-            alignSelf='center'
-            color='text.primary'
-            flexGrow={1}
+    <>
+      <Collapse in={preloadedDesignID > 0}>
+        <BosonHeader>
+          <Stack
+            direction='row'
+            width='100%'
+            sx={{ opacity: preloadedDesignID > 0 ? 1 : 0 }}
           >
-            Design {preloadedDesignID}
-            {isCloned && ' (CLONED)'} has been preloaded.
-          </Typography>
-          <Button
-            variant='outlined'
-            size='small'
-            sx={(theme) => ({
-              '&.MuiButton-root': {
-                color: 'text.secondary',
-                border: `1px solid ${theme.palette.text.secondary}`,
-              },
-            })}
-            onClick={loadDesign}
-          >
-            Load
-          </Button>
-          <IconButton
-            sx={{ ml: 1 }}
-            size='small'
-            disableRipple
-            onClick={clearDesign}
-          >
-            <ClearIcon fontSize='inherit' />
-          </IconButton>
-        </Stack>
-      </BosonHeader>
-    </Collapse>
+            <Typography
+              fontSize={16}
+              fontWeight={400}
+              textAlign='center'
+              alignSelf='center'
+              color='text.primary'
+              flexGrow={1}
+            >
+              Design {preloadedDesignID}
+              {isCloned && ' (CLONED)'} has been preloaded.
+            </Typography>
+            <Button
+              variant='outlined'
+              size='small'
+              sx={(theme) => ({
+                '&.MuiButton-root': {
+                  color: 'text.secondary',
+                  border: `1px solid ${theme.palette.text.secondary}`,
+                },
+              })}
+              onClick={checkAndLoad}
+            >
+              Load
+            </Button>
+            <IconButton
+              sx={{ ml: 1 }}
+              size='small'
+              disableRipple
+              onClick={clearDesign}
+            >
+              <ClearIcon fontSize='inherit' />
+            </IconButton>
+          </Stack>
+        </BosonHeader>
+      </Collapse>
+      <SnackAlert
+        ref={preloadAlertRef}
+        message='An exposure is running'
+        actionText='LOAD'
+        action={loadDesign}
+      />
+    </>
   );
 }
