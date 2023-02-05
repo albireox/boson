ae122f7a93914dfc58abbcdfc631102ac63670aa
José Sánchez-Gallego <gallegoj@uw.edu>
2023-02-04 16:21:29 -0800

    Add SnackAlert component

---
+++ src/renderer/HAL/Components/SnackAlert.tsx
@@ -0,0 +1,107 @@
+/*
+ *  @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+ *  @Date: 2023-02-04
+ *  @Filename: SnackAlert.tsx
+ *  @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+ */
+
+import CloseIcon from '@mui/icons-material/Close';
+import { Alert, AlertProps, Button, IconButton, Snackbar } from '@mui/material';
+import { Stack } from '@mui/system';
+import React from 'react';
+
+export interface SnackAlertRefType {
+  open: () => void;
+}
+
+interface SnackAlertProps {
+  message: string;
+  severity?: AlertProps['severity'];
+  autoHideDuration?: null | number;
+  actionText?: string;
+  action?: () => void;
+}
+
+interface ActionButtonProps {
+  message: string;
+  action?: () => void;
+}
+
+function ActionButton(props: ActionButtonProps) {
+  const { message, action } = props;
+
+  return (
+    <Button
+      color='inherit'
+      size='small'
+      onClick={action}
+      sx={{ minWidth: '32px' }}
+      // sx={{ px: '2px', py: '1px' }}
+    >
+      {message}
+    </Button>
+  );
+}
+
+const SnackAlert = React.forwardRef<SnackAlertRefType, SnackAlertProps>(
+  (props, ref) => {
+    const {
+      message,
+      autoHideDuration = null,
+      severity = 'warning',
+      actionText,
+      action,
+    } = props;
+
+    const [open, setOpen] = React.useState(false);
+
+    React.useImperativeHandle(ref, () => ({
+      open: () => setOpen(true),
+    }));
+
+    const handleClose = React.useCallback(() => setOpen(false), []);
+
+    return (
+      <Snackbar
+        open={open}
+        autoHideDuration={autoHideDuration}
+        onClose={handleClose}
+      >
+        <Alert
+          elevation={6}
+          variant='filled'
+          onClose={handleClose}
+          severity={severity}
+          sx={{
+            width: '100%',
+            '& .MuiAlert-message': { alignSelf: 'center' },
+            '& .MuiAlert-action': {
+              padding: '0px 0px 0px 16px',
+              placeItems: 'center',
+            },
+          }}
+          action={
+            <Stack direction='row' spacing={0}>
+              {actionText && (
+                <ActionButton
+                  message={actionText}
+                  action={() => {
+                    if (action !== undefined) action();
+                    handleClose();
+                  }}
+                />
+              )}
+              <IconButton onClick={handleClose} color='inherit' sx={{ p: 0.5 }}>
+                <CloseIcon sx={{ fontSize: '16px' }} />
+              </IconButton>
+            </Stack>
+          }
+        >
+          {message}
+        </Alert>
+      </Snackbar>
+    );
+  }
+);
+
+export default SnackAlert;
