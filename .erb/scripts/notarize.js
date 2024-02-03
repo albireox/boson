bb51076fa8f252ac77a0e2031000c7dc8818719e
José Sánchez-Gallego <gallegoj@uw.edu>
2024-02-03 13:30:21 -0800

    Use teamId

--- .erb/scripts/notarize.js
+++ .erb/scripts/notarize.js
@@ -26,6 +26,6 @@ exports.default = async function notarizeMacos(context) {
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLE_ID,
     appleIdPassword: process.env.APPLE_ID_PASS,
-    ascProvider: process.env.ASCPROVIDER,
+    teamId: process.env.APPLE_TEAM_ID,
   });
 };
