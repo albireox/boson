ad622e913a69d315f702ff7abbf46d121f791fe5
José Sánchez-Gallego <gallegoj@uw.edu>
2024-02-03 09:38:59 -0800

    Update ERB scripts

--- .erb/scripts/notarize.js
+++ .erb/scripts/notarize.js
@@ -7,14 +7,16 @@ exports.default = async function notarizeMacos(context) {
     return;
   }
 
-  if (process.env.CI !== 'true' && process.env.DOPACK !== '1') {
+  if (process.env.CI !== 'true') {
     console.warn('Skipping notarizing step. Packaging is not running in CI');
     return;
   }
 
-  if (!('APPLE_ID' in process.env && 'APPLE_ID_PASS' in process.env)) {
+  if (
+    !('APPLE_ID' in process.env && 'APPLE_APP_SPECIFIC_PASSWORD' in process.env)
+  ) {
     console.warn(
-      'Skipping notarizing step. APPLE_ID and APPLE_ID_PASS env variables must be set'
+      'Skipping notarizing step. APPLE_ID and APPLE_APP_SPECIFIC_PASSWORD env variables must be set',
     );
     return;
   }
@@ -25,6 +27,6 @@ exports.default = async function notarizeMacos(context) {
     appBundleId: build.appId,
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLE_ID,
-    appleIdPassword: process.env.APPLE_ID_PASS,
+    appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
   });
 };
