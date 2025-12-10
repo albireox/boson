5e8b83d1cc40f70beaeeeb5f80ad80c5e97eec62
legosp7 <37090462+legosp7@users.noreply.github.com>
2025-12-09 21:34:29 -0700

    header starter

---
+++ src/renderer/Status/Header/Header.tsx
@@ -0,0 +1,5 @@
+import { Stack }  from '@mui/material';
+import { Box } from '@mui/system';
+import statuslogo from '../images/statuslogo.png';
+import { DesignInput } from './DesignInput';
+import { PreloadedBanner } from './PreloadedBanner';
