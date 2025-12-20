e292f285fc05518aa07cc18fdc08dde4c79bef41
legosp7 <37090462+legosp7@users.noreply.github.com>
2025-12-19 18:41:15 -0700

    sutff

--- src/renderer/Status/DesignInput.tsx
+++ src/renderer/Status/DesignInput.tsx
@@ -7,8 +7,10 @@ export function DesignInput() {
     const keywords = useKeywordContext();
     const { configuration_loaded: configurationLoaded } = keywords.state;
 
-    const[Az, setAzID ] = React.useState<string>('');
-    
+    const[Az, setAzID ] = React.useState<string>('0:00:00.00');
+    const [Alt, setAltID ] = React.useState<string>('0:00:00.00');
+    const [Rot, setRotID ] = React.useState<string>('0.00');
+
 
 
 } 
