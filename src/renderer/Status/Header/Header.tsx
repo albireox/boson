753e86a7517f90839f890bca2e0b2b521894076e
legosp7 <37090462+legosp7@users.noreply.github.com>
2026-01-14 19:28:45 -0700

    small update

--- src/renderer/Status/Header/Header.tsx
+++
@@ -1,3 +0,0 @@
-import { Stack }  from '@mui/material';
-import { Box } from '@mui/system';
-import statuslogo from '../images/statuslogo.png';
--- src/renderer/Status/Status.tsx
+++ src/renderer/Status/Status.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 import { CssBaseline, Box, Stack } from '@mui/material';
-import { useKeywordContext } from 'renderer/hooks';
+import { KeywordContext, useKeywords } from 'renderer/hooks';
+import StatusHeader from './Header/StatusHeader';
+import StatusPanels from './Header/StatusPanels';
+
 
 function MyButton({ title }: { title: string }) {
     return <Button variant="contained">{title}</Button>;
     }
 
 export default function Status() {
+
+    const statusKeywords = useKeywords([
+        'status.coordsys_ICRS',
+        'status.coordsys_FK5',
+        'status.coordsys_FK4',
+        'status.coordsys_galactic',
+        'status.coordsys_geocentric',
+        'status.coordsys_topocentric',
+        'status.coordsys_observed',
+        'status.coordsys_physical',
+        'status.coordsys_mount',
+        'jaeger.configuration_loaded',
+        'jaeger.design_preloaded',
+        'jaeger.preloaded_is_cloned',
+    ])
+
     return (
         <Box
         component='main'
