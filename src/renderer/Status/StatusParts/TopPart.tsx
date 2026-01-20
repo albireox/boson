2170c16e04b6d24fcec1eb06eed821fa36ead3f9
legosp7 <37090462+legosp7@users.noreply.github.com>
2026-01-19 21:10:38 -0700

    update

---
+++ src/renderer/Status/StatusParts/TopPart.tsx
@@ -0,0 +1,7 @@
+import { Box } from '@mui/system';
+import React from 'react';
+import { useKeywordContext } from 'renderer/hooks';
+
+export default function TopPart() {
+    const keywords = useKeywordContext();
+}
